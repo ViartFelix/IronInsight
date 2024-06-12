c1f6eae051000c7ebd5ffe2cd27ae48ffb80846b
RaphaelAZ <azevedo.raphael2017@gmail.com>
2024-06-12 12:42:33 +0200

    (+) routes added

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ExercisesComponent } from './exercises/exercises.component';
+import { ProgramsComponent } from './programs/programs.component';
+import { ContactComponent } from './contact/contact.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +13,17 @@ export const routes: Routes = [
     {
         path: 'exercices',
         component: ExercisesComponent
+    },
+    {
+        path: 'programmes',
+        component: ProgramsComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
+    {
+        path: 'inscription',
+        component: RegisterComponent
     }
 ];
