a5ce0cd5cafd177d00fd32ebcd36cbfdfaba0305
Félix VIART <viart.felix2017@gmail.com>
2024-07-02 21:03:36 +0200

    (+) wip profile

--- front/src/app/profile/profile.component.ts
+++ front/src/app/profile/profile.component.ts
@@ -3,21 +3,45 @@ import { RouterModule } from '@angular/router';
 import { ProfileService } from '../../services/profile.service';
 import { Record } from '../../models/Record';
 import { CommonModule } from '@angular/common';
-import { map } from 'rxjs';
+import AuthService from "../../services/auth.service";
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle, MatCardSubtitle } from "@angular/material/card";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardContent
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-  record: Record = {} as Record;
+  private _records: Record[] = [];
+  public hasLoaded: boolean = false;
 
-  constructor(private profileService: ProfileService) {}
+  constructor(
+    private profileService: ProfileService,
+    private authService: AuthService,
+  ) {}
 
   ngOnInit() {
-    this.profileService.getRecords('2').subscribe((record: Record) => this.record = record);
+    //take the current auth ID
+    const profile_id: number = this.authService.user.value.id_user
+    const strProfile = profile_id.toString()
+
+    this.profileService
+      .getRecords(strProfile)
+      .subscribe((records: Record[]) => {
+        this._records = records
+        this.hasLoaded = true;
+      });
   }
+
+  get records(): Record[] { return this._records; }
 }
